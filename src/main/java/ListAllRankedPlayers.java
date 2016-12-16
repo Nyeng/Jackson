0c8d374626489f2c4e203ffb56a5d406d192096c
Nyeng <k79689@skatteetaten.no>
2016-12-16 10:03:16 +0100

    minor structure change

---
+++ src/main/java/ListAllRankedPlayers.java
@@ -0,0 +1,6 @@
+/**
+ * Created by k79689 on 16.12.16.
+ */
+public class ListAllRankedPlayers {
+
+}
--- src/main/java/ServePlayerInfo.java
+++ src/main/java/ServePlayerInfo.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.URL;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -8,29 +9,183 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class ServePlayerInfo {
 
-    String api_endpoint = "http://smashranking.eu/api/smashers/v-dogg-no/";
+    private ObjectMapper mapper = new ObjectMapper();
 
-    public String getApi_endpoint() {
-        return api_endpoint;
+    private Player getPlayerInfo() throws IOException {
+        return mapper.readValue(new URL(getApi_endpoint()), Player.class);
     }
 
+    private String getApi_endpoint() {
+        return "http://smashranking.eu/api/smashers/v-dogg-no/";
+    }
 
     public static void main(String[] args) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
         ServePlayerInfo playainfo = new ServePlayerInfo();
-        Player playa = new Player();
 
-
-      //  Staff obj = mapper.readValue(new URL("http://mkyong.com/api/staff.json"), Staff.class);
-        Player object = mapper.readValue(new URL(playainfo.getApi_endpoint()), Player.class);
-        System.out.println(object.getName());
+        System.out.println(playainfo.returnPlayerInfo());
     }
 
+    private String returnPlayerInfo() throws IOException {
+        String playerInfo = null;
 
-    public String getAllinfo(String json){
+        try {
+            Player player = getPlayerInfo();
 
-        return "";
+            playerInfo = ("Player info"
+                + "\nName: " + player.name
+                + "\nRank " + player.getEurank()
+                + "\nCountry rank " + player.getCountry_rank()
+            );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return playerInfo;
+
+    }
+
+}
+
+/**
+ * Created by k79689 on 15.12.16.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Player {
+
+    boolean active;
+    String tag;
+    String team;
+    String name;
+    String nationality;
+    String country;
+    String city;
+    String region;
+    String main;
+    String secondary;
+    String tertiary;
+    String quaternary;
+    String tournaments;
+    Integer wins;
+    Integer losses;
+    Double ratio;
+    Double mu;
+    Double sigma;
+    Double ts;
+    String facebook;
+    String twitter;
+    String twitch;
+    String youtube;
+    Integer eurank;
+    Integer country_rank;
+    Integer character_rank;
+
+    public Player() { }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public String getSecondary() {
+        return secondary;
+    }
+
+    public String getTertiary() {
+        return tertiary;
+    }
+
+    public String getQuaternary() {
+        return quaternary;
+    }
+
+    public String getTournaments() {
+        return tournaments;
+    }
+
+    public Integer getWins() {
+        return wins;
+    }
+
+    public Integer getLosses() {
+        return losses;
+    }
+
+    public Double getRatio() {
+        return ratio;
+    }
+
+    public Double getMu() {
+        return mu;
+    }
+
+    public Double getSigma() {
+        return sigma;
+    }
+
+    public Double getTs() {
+        return ts;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public String getTwitch() {
+        return twitch;
+    }
+
+    public String getYoutube() {
+        return youtube;
+    }
+
+    public Integer getEurank() {
+        return eurank;
+    }
+
+    public Integer getCountry_rank() {
+        return country_rank;
+    }
+
+    public Integer getCharacter_rank() {
+        return character_rank;
     }
 
 
 }
+
